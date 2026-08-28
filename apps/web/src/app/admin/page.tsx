c62e265c07b6d707edce17346e516b01dfd5fffe
U <aps.apisit250@gmail.com>
2026-08-28 14:01:01 +0700

    feat: update package.json scripts and remove unused files from UI components

---
+++ apps/web/src/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AdminLayout from '@/shared/components/layouts/admin-layout';
+
+export default function Page() {
+  return (
+    <AdminLayout>Page</AdminLayout>
+  )
+}
