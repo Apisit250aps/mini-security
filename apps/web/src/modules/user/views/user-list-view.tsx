be3261f52a57133d04a7b1795cb6531295c35aa4
U <aps.apisit250@gmail.com>
2026-08-30 14:11:24 +0700

    feat: create UserCreateAction component for user creation dialog

--- apps/web/src/modules/user/views/user-list-view.tsx
+++ apps/web/src/modules/user/views/user-list-view.tsx
@@ -1,26 +1,12 @@
 'use client';
 import React from 'react';
 import UserDataTable from '../components/table/user-data-table';
-import UserCreateForm from '../components/form/user-create-form';
 import PageLayout from '@/shared/components/layouts/page-layout';
-import { useOverlay } from '@repo/ui/hooks';
-import { Button } from '@repo/ui/components/button';
+import UserCreateAction from '../components/user-create-action';
 
 export default function UserListView() {
-  const ui = useOverlay();
-  const createAction = () => {
-    ui.dialog.open({
-      title: 'เพิ่มผู้ใช้',
-      description: 'กรุณากรอกข้อมูลผู้ใช้ใหม่',
-      children: <UserCreateForm />,
-    });
-  };
-
   return (
-    <PageLayout
-      pageId="user"
-      actions={<Button onPress={createAction}>เพิ่มผู้ใช้</Button>}
-    >
+    <PageLayout pageId="user" actions={<UserCreateAction />}>
       <UserDataTable />
     </PageLayout>
   );
