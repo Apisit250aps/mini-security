e38f7748b6217cf36f177a0d215b8f805e1ab90e
U <aps.apisit250@gmail.com>
2026-09-12 12:01:47 +0700

    feat: Set up Docker environment with multi-stage builds for API and Web apps, add .env.example and .dockerignore

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 import config from '@repo/configs';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   /* config options here */
   reactCompiler: true,
   experimental: {
